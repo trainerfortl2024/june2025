64d5029026242b83fe296e23e97c2a5c3e36e9f0
suresh <trainerfortl2024@gmail.com>
2025-07-13 16:47:47 +0530

    updated

--- nodejs/http/serverdemo1.js
+++ nodejs/http/serverdemo1.js
@@ -1,10 +1,21 @@
 const http = require('http')
 
-const server = http.createServer((req,res)=>{
-    res.writeHead(200,{'Content-Type':'text/html'})
-    res.end('Hello <b>Node.js</b>')
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+
+    console.log(req.url)
+    if (req.url === '/home')
+        res.write('<h1>Welcome to the Home page</h1>')
+
+    else if (req.url === '/node') {
+        res.write('Hello <b>Node.js</b>')
+    }
+    else {
+        res.write('loading..')
+    }
+    res.end();
 
 })
-server.listen('4000', ()=>{
-    console.log('server started - http://localhost:4000')
+server.listen('4000', () => {
+    console.log('server started - http://localhost:4000/')
 })
