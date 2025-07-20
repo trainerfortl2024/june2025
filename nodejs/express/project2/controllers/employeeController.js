8c72e032b75068ac3f19ce6a749f2ffbb226c81c
suresh <trainerfortl2024@gmail.com>
2025-07-20 15:13:45 +0530

    updated node.js

--- nodejs/express/project2/controllers/employeeController.js
+++ nodejs/express/project2/controllers/employeeController.js
@@ -4,37 +4,65 @@ import { employeeDetails } from '../data/employeeData.js'
 
 
 //read
-const getEmployees = (req,res) => {
+const getEmployees = (req, res) => {
     res.json(employeeDetails)
 }
 
-const getEmployeeById = (req,res) => {
+const getEmployeeById = (req, res) => {
     const id = parseInt(req.params.id);
-    const employee = employeeDetails.find( emp => emp.id === id ) 
-    if(employee){
-    res.json(employee)
+    const employee = employeeDetails.find(emp => emp.id === id)
+    if (employee) {
+        res.json(employee)
     }
-    else{
-        res.status(404).json({message:'Employee not found'})
+    else {
+        res.status(404).json({ message: 'Employee not found' })
     }
 }
 
-const addEmployee = (req,res) => {
+const addEmployee = (req, res) => {
     const newId = employeeDetails.length + 1
 
     const newEmployee = {
-        id:newId,
-        name:req.body.name,
-        salary:req.body.salary
+        id: newId,
+        name: req.body.name,
+        salary: req.body.salary
     }
     employeeDetails.push(newEmployee);
     res.status(201).json(newEmployee)
 }
 
-const updateEmployee = (req,res) => {
+const updateEmployee = (req, res) => {
+    const empId = parseInt(req.params.id);
+    const index = employeeDetails.findIndex(e => e.id === empId);
+
+    if (index !== -1) {
+        const updatedEmp = {
+            id: empId,
+            name: req.body.name,
+            salary: req.body.salary
+        };
+        employeeDetails[index] = updatedEmp;
+        res.json(updatedEmp);
+    }
+    else {
+        res.status(404).json({ message: "Employee not found" });
+    }
+
+}
+
+const deleteEmployee = (req, res) => {
+    const empId = parseInt(req.params.id);
+    const index = employeeDetails.findIndex(e => e.id === empId);
+    if (index !== -1) {
+        employeeDetails.splice(index, 1)
+        res.json({ message: "Employee Deleted Successfully" })
+    }
+    else {
+        res.status(404).json({ message: "Employee not found" });
+    }
 
 
 }
 
 
-export {getEmployees,getEmployeeById,addEmployee}
+export { getEmployees, getEmployeeById, addEmployee, updateEmployee,deleteEmployee }
--- nodejs/express/project2/routes/employeeRoutes.js
+++ nodejs/express/project2/routes/employeeRoutes.js
@@ -1,10 +1,11 @@
-import { getEmployees,getEmployeeById,addEmployee,updateEmployee } from "../controllers/employeeController.js"
+import { getEmployees,getEmployeeById,addEmployee,updateEmployee,deleteEmployee } from "../controllers/employeeController.js"
 
 const routes = (app) => {
     app.get('/employee',getEmployees)
     app.get('/employee/:id',getEmployeeById)
     app.post('/employee',addEmployee)
     app.put('/employee/:id',updateEmployee)
+    app.delete('/employee/:id',deleteEmployee)
 
 }
 export {routes}
